9ecb00d7424cdf1a346685c6ae4812939a961dc8
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2022-11-03 21:06:51 +0200

    Update src version: 1.2.9 -> 1.2.9

--- src/vcf-autocomplete.js
+++ src/vcf-autocomplete.js
@@ -109,7 +109,7 @@ class VcfAutocomplete extends ElementMixin(ThemableMixin(mixinBehaviors([IronRes
   }
 
   static get version() {
-    return '1.2.8';
+    return '1.2.9';
   }
 
   static get properties() {
